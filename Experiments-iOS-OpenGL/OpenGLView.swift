abaf98456121ff0d1b9be2099e0eca9f3548255b
Andrew <andrew.ashurov@spalmalo.com>
2017-08-23 16:33:51 +0600

    added shaders

--- Experiments-iOS-OpenGL/OpenGLView.swift
+++ Experiments-iOS-OpenGL/OpenGLView.swift
@@ -9,6 +9,24 @@ import UIKit
 import QuartzCore
 import OpenGLES
 
+
+struct Vertex {
+    var Position: (CFloat, CFloat, CFloat)
+    var Color: (CFloat, CFloat, CFloat, CFloat)
+}
+
+var Vertices = [
+    Vertex(Position: (1, -1, 0) , Color: (1, 0, 0, 1)),
+    Vertex(Position: (1, 1, 0)  , Color: (0, 1, 0, 1)),
+    Vertex(Position: (-1, 1, 0) , Color: (0, 0, 1, 1)),
+    Vertex(Position: (-1, -1, 0), Color: (0, 0, 0, 1))
+]
+
+var Indices: [GLubyte] = [
+    0, 1, 2,
+    2, 3, 0
+]
+
 class OpenGLView:UIView{
     var
     eaglLayer:CAEAGLLayer!,
@@ -36,6 +54,8 @@ class OpenGLView:UIView{
         self.setupContext()
         self.setupRenderBuffer()
         self.setupFrameBuffer()
+        self.compileShaders()
+        self.setupVBOs()
         self.render()
     }
     
@@ -43,9 +63,8 @@ class OpenGLView:UIView{
         eaglLayer = layer as! CAEAGLLayer
         eaglLayer.isOpaque = true
     }
-    
     private func setupContext(){
-        if let context = EAGLContext(api: .openGLES3) {
+        if let context = EAGLContext(api: .openGLES2) {
             self.context = context
             if !EAGLContext.setCurrent(context) {
                 preconditionFailure("Failed to set current opengl context")
@@ -54,27 +73,125 @@ class OpenGLView:UIView{
             preconditionFailure("failed to initial opengl context")
         }
     }
-    
     private func setupRenderBuffer() {
         glGenRenderbuffers(1, &colorRenderBuffer)
         glBindRenderbuffer(GLenum(GL_RENDERBUFFER), colorRenderBuffer)
         context.renderbufferStorage(Int(GL_RENDERBUFFER), from: eaglLayer)
     }
-    
     private func setupFrameBuffer() {
         var frameBuffer: GLuint = 0
         glGenFramebuffers(1, &frameBuffer)
         glBindFramebuffer(GLenum(GL_FRAMEBUFFER), frameBuffer)
         glFramebufferRenderbuffer(GLenum(GL_FRAMEBUFFER), GLenum(GL_COLOR_ATTACHMENT0), GLenum(GL_RENDERBUFFER), colorRenderBuffer)
     }
-    
     private func render() {
+        //background color
         glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0)
         glClear(GLbitfield(GL_COLOR_BUFFER_BIT))
-        let success = context?.presentRenderbuffer(Int(GL_RENDERBUFFER))
-        if !success!{
+        
+        renderShaders()
+        
+        let success = context.presentRenderbuffer(Int(GL_RENDERBUFFER))
+        if !success{
             preconditionFailure("failed to presentRenderbuffer")
         }
     }
     
+    //Shaders
+    var
+    positionSlot: GLuint = 0,
+    colorSlot: GLuint = 0
+    private func compileShader(type: GLenum ,shaderName: String!) -> GLuint{
+        
+        var shader : GLuint!
+        var status : GLint = 0
+        var source : UnsafePointer<GLchar>?
+        
+        let shaderFolder = "shaders/"
+        
+        let shaderPath = Bundle.main.path(forResource: shaderFolder + shaderName, ofType: "glsl")!
+        let ShaderString = try! String(contentsOfFile: shaderPath, encoding: .utf8)
+        
+        
+        source = UnsafePointer<GLchar>(ShaderString)
+        var sourceLength = GLint(ShaderString.lengthOfBytes(using: String.Encoding.utf8))
+        if source == nil {
+            preconditionFailure("failed to compileShader: source == nil")
+        }
+        
+        shader  = glCreateShader(type)
+        let cString = ShaderString.cString(using: String.Encoding.utf8)
+        var tempString : UnsafePointer<GLchar>? =  UnsafePointer<GLchar>(cString)
+        glShaderSource(shader, 1,&tempString, &sourceLength)
+        glCompileShader(shader)
+        
+        glGetShaderiv(shader, GLenum(GL_COMPILE_STATUS), &status)
+        
+        if status == GL_FALSE {
+            
+            var length: GLint = 0;
+            glGetShaderiv(shader, GLenum(GL_INFO_LOG_LENGTH), &length);
+            
+            let errorLog = unsafeBitCast(malloc(Int(length) * MemoryLayout<GLchar>.size), to:UnsafeMutablePointer<GLchar>.self)
+            glGetShaderInfoLog(shader, length, &length, errorLog)
+            
+            let err = String(cString: errorLog)
+            
+            free(errorLog)
+            glDeleteShader(shader)
+            
+            preconditionFailure("failed to compileShader: \(err)")
+        }
+        
+        return shader
+    }
+    
+    private func compileShaders() {
+        
+        // Compile our vertex and fragment shaders.
+        let
+        vertexShader = compileShader(type: GLenum(GL_VERTEX_SHADER), shaderName: "SimpleVertex"),
+        fragmentShader = compileShader(type: GLenum(GL_FRAGMENT_SHADER), shaderName: "SimpleFragment")
+        
+        // Call glCreateProgram, glAttachShader, and glLinkProgram to link the vertex and fragment shaders into a complete program.
+        let programHandle: GLuint = glCreateProgram()
+        glAttachShader(programHandle, vertexShader)
+        glAttachShader(programHandle, fragmentShader)
+        glLinkProgram(programHandle)
+        
+        // Check for any errors.
+        var linkSuccess: GLint = GLint()
+        glGetProgramiv(programHandle, GLenum(GL_LINK_STATUS), &linkSuccess)
+        if (linkSuccess == GL_FALSE) {
+            preconditionFailure("Failed to create shader program!")
+        }
+        
+        // Call glUseProgram to tell OpenGL to actually use this program when given vertex info.
+        glUseProgram(programHandle)
+        
+        // Finally, call glGetAttribLocation to get a pointer to the input values for the vertex shader, so we
+        //  can set them in code. Also call glEnableVertexAttribArray to enable use of these arrays (they are disabled by default).
+        self.positionSlot = GLuint(glGetAttribLocation(programHandle, "Position"))
+        self.colorSlot = GLuint(glGetAttribLocation(programHandle, "SourceColor"))
+        glEnableVertexAttribArray(self.positionSlot)
+        glEnableVertexAttribArray(self.colorSlot)
+    }
+    private func setupVBOs() {
+        var vertexBuffer: GLuint = 0
+        glGenBuffers(1, &vertexBuffer)
+        glBindBuffer(GLenum(GL_ARRAY_BUFFER), vertexBuffer)
+        glBufferData(GLenum(GL_ARRAY_BUFFER), (Vertices.count * MemoryLayout<Vertex>.size), Vertices, GLenum(GL_STATIC_DRAW))
+        
+        var indexBuffer: GLuint = 0
+        glGenBuffers(1, &indexBuffer)
+        glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), indexBuffer)
+        glBufferData(GLenum(GL_ELEMENT_ARRAY_BUFFER), (Indices.count * MemoryLayout<GLubyte>.size), Indices, GLenum(GL_STATIC_DRAW))
+    }
+    private func renderShaders(){
+        //shaders
+        glViewport(0, 0, GLint(self.frame.size.width), GLint(self.frame.size.height));
+        glVertexAttribPointer(positionSlot, 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(MemoryLayout<Vertex>.size), nil)
+        glVertexAttribPointer(colorSlot, 4, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(MemoryLayout<Vertex>.size), UnsafePointer<Float>(bitPattern: 3 * MemoryLayout<Float>.size))
+        glDrawElements(GLenum(GL_TRIANGLES), GLsizei(Indices.count/MemoryLayout.size(ofValue: Indices[0])), GLenum(GL_UNSIGNED_BYTE), nil)
+    }
 }
